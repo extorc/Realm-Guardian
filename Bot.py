b19ca23e293a9c0149d6e79a7ce329bdf37286af
Shaurya Mittal <mittalshaurya004@gmail.com>
2021-10-22 10:51:33 +0530

    Utils

--- Bot.py
+++ Bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import requests
 from dotenv import load_dotenv,dotenv_values
-from utils import check_if_url,getReply,TOKEN
+from utils import check_if_url,getReply,TOKEN,formattingMessage,formattedCodeImageLink
 
 bot = commands.Bot(command_prefix="~")
 
@@ -19,14 +19,10 @@ def check_if_url(link):
 
 @bot.command()
 async def format(ctx):
-    formattingMessage = """Hey <@%s> \n
-    A Message that you have sent contains code but is unformatted.\n
-    This means that you message is difficult to be read by others in the server.\n
-    Kindly suround your message in ` 3 times so that it could appear like ```this```"""
     msg = await getReply(ctx)
     embed = discord.Embed(title = "Please Format your code in the Message"
     ,description = formattingMessage % msg.author.id)
-    embed.set_image(url = "https://media.discordapp.net/attachments/877232296497905667/900783733987233792/unknown.png")
+    embed.set_image(url = formattedCodeImageLink)
     await ctx.send(embed = embed)    
 
 @bot.command()
--- utils.py
+++ utils.py
@@ -4,6 +4,13 @@ from dotenv import load_dotenv,dotenv_values
 temp = dotenv_values(".env")
 TOKEN = temp["TOKEN"]
 
+formattingMessage = """Hey <@%s> \n
+    A Message that you have sent contains code but is unformatted.\n
+    This means that you message is difficult to be read by others in the server.\n
+    Kindly suround your message in ` 3 times so that it could appear like ```this```"""
+
+formattedCodeImageLink = "https://media.discordapp.net/attachments/877232296497905667/900783733987233792/unknown.png"
+
 def check_if_url(link):
     try:
         site=requests.get(link)
